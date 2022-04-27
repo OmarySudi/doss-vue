fd786722d1e375788d9f464393ad7fec60935ffc
sudi omary <kekovasudi@gmail.com>
2022-04-27 15:02:44 +0300

    not showing head teacher when viewing school

--- src/views/schools/School.vue
+++ src/views/schools/School.vue
@@ -39,11 +39,11 @@
                             </v-row>
 
                             <v-row>
-                                 <v-col cols="12" sm="6">
+                                 <!-- <v-col cols="12" sm="6">
                                     <div class="py-1 px-1">
-                                        <span>HEAD TEACHER : </span> <span v-if="school.headTeacher != null">{{ school.headTeacher.teacher.name }}</span>
+                                        <span>HEAD TEACHER : </span> <span v-if="school.headTeacher != null">{{ school.headTeacher.name }}</span>
                                     </div>
-                                </v-col>
+                                </v-col> -->
 
                                 <v-col cols="12" sm="6">
                                     <div class="py-1 px-1">
