f61888482c8ca286e5be530782ea29f59d7f2c33
sudi omary <kekovasudi@gmail.com>
2022-07-19 08:59:00 +0300

    change api address

--- src/main.js
+++ src/main.js
@@ -11,9 +11,9 @@ Vue.config.productionTip = false
 
 Vue.use(Vuelidate);
 
-//ApiService.init("http://localhost:8000/api")
+ApiService.init("http://localhost:8000/api")
 // ApiService.init("http://ringlesoft.com:8007/api")
-ApiService.init("https://e382-197-250-100-22.ngrok.io/api")
+ApiService.init("https://e824-155-12-100-67.ngrok.io/api")
 
 
 require('@/store/subscriber');
