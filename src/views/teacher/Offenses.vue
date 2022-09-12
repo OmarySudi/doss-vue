401d43aa04fa12dfdf35edb939a93b4a11ddc962
sudi omary <kekovasudi@gmail.com>
2022-09-12 04:11:06 +0300

    closing an offense

--- src/views/teacher/Offenses.vue
+++ src/views/teacher/Offenses.vue
@@ -1,5 +1,12 @@
 <template>
     <v-container>
+        <Snackbar 
+            :type="snackbarType" 
+            :snackbar="snackbar" 
+            :text="snackbarText" 
+            :timeout="snackbarTimeout"
+        />
+
         <v-card class="mt-3">
             <v-container fluid>
                 <v-row>
@@ -37,7 +44,17 @@
                                     class="ml-3"
                                     @click="viewOffense(item)"
                                 >
-                                    <span style="color:white">VIEW DETAILS</span>
+                                    <span style="color:white">VIEW</span>
+                                </v-btn> 
+
+                                <v-btn 
+                                    color="blue" 
+                                    small 
+                                    class="ml-3"
+                                    :disabled="item.resolution != null"
+                                    @click="closeOffense(item)"
+                                >
+                                    <span style="color:white">CLOSE</span>
                                 </v-btn> 
                         </template>
 
@@ -100,10 +117,24 @@
                         </v-row>
                     </v-card-text>
                 </v-card>
-                <v-card>
+                <v-card elevation="3" class="my-3 mx-3">
                     <v-card-subtitle class="text-center font-weight-bold">OFFENSE RESOLUTION</v-card-subtitle>
-                    <v-card-text>
+                    <v-card-text v-if="offense.resolution !=null">
+                        <v-row>
+                            <v-col cols="12">
+                                <v-row>
+                                    <v-col cols="6"><span class="font-weight-medium">Action:</span><span class="ml-2">{{offense.resolution.action}}</span></v-col>
+                                    <v-col cols="6"><span class="font-weight-medium">Status:</span><span class="ml-2">{{offense.resolution.status}}</span></v-col>
+                                </v-row>
+                            </v-col>
 
+                            <v-col cols="12" v-if="offense.resolution.description !=null">
+                                <span class="font-weight-medium">Description:</span>
+                                <span>
+                                    {{offense.resolution.description}}
+                                </span>
+                            </v-col>
+                        </v-row>
                     </v-card-text>
                 </v-card>
 
@@ -112,26 +143,118 @@
                 </v-card-actions>
             </v-card>
         </v-dialog>
+
+
+        <v-dialog
+            v-model="close_dialog"
+            max-width="620px"    
+        >
+            <v-card
+                outlined
+                elevation="4"
+            >
+                <v-card-subtitle 
+                    class="text-center font-weight-bold"
+                >
+                    OFFENSE RESOLUTION
+                </v-card-subtitle>
+
+                <v-card-text>
+                    <v-row>
+
+                        <v-col cols="12">
+                            <v-select
+                                :items="statuses"
+                                label="Status"
+                                v-model="resolution_status"
+                            ></v-select>
+                        </v-col>
+
+                        <v-col cols="12">
+                            <v-select
+                                :items="actions"
+                                label="Action"
+                                v-model="resolution_action"
+                            ></v-select>
+                        </v-col>
+
+                        <v-col cols="12">
+                            <v-textarea
+                                v-model="resolution_description"
+                                label="Description"
+                                rows="3"
+                            ></v-textarea>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="close_dialog = false"
+                    >
+                    Cancel
+                    </v-btn>
+
+                    <v-btn
+                        color="blue darken-1"
+                        class="ml-2 mr-5"
+                        text
+                        @click="saveOffenseResolution"
+                    >
+                    Save
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <CircularLoader :loading="circularLoader"/>
     </v-container>
 </template>
 
 <script>
 import ApiService from '../../services/api'
 import { mapGetters,mapActions } from 'vuex';
+import {projectMixin} from '../../mixins/mixins'
+import Snackbar from '../../components/Snackbar.vue'
+import CircularLoader from '../../components/CircularLoader.vue'
+
 
 export default {
+
+    components: {Snackbar,CircularLoader},
+    
+    mixins: [projectMixin],
+
     data: ()=>({
 
         offense_occurrences:[],
         load_offense_occurrences: true,
         selected_offense: {},
+        close_dialog: false,
         message_sent_icon: "",
         message_sent_icon_color: "",
         message_reply_icon: "",
         message_reply_icon_color:"",
+        resolution_action:"",
+        resolution_description:"",
+        resolution_status: "CLOSED",
+
+        actions: [
+            "School dropout",
+            "School dismissal",
+            "School continuation"
+        ],
+
+        statuses:[
+            "CLOSED",
+            "OPEN"
+        ],
 
 
         offense: {
+            id:"",
             name:"",
             description: "",
             action: "",
@@ -170,16 +293,61 @@ export default {
             this.load_offense_occurrences = false;
         },
 
+        closeOffense(item){
+            this.offense.id = item.id;
+            this.close_dialog = true;
+        },
+
+        saveOffenseResolution(){
+
+            let data = {
+                offence_occurance_id: this.offense.id,
+                status: this.resolution_status,
+                action: this.resolution_action
+            }
+
+            this.close_dialog = false;
+            this.circularLoader = true;
+
+            ApiService.post('/offence-resolutions/',data).then((response)=>{
+                if(response.status == 200){
+                    this.circularLoader = false;
+                    this.setAlert("success",true,response.data.message,5000);
+                    this.setOffenseOccurrences();
+                } else {
+
+                    if(response.data.objects){
+
+                    this.circularLoader = false;
+                    this.setAlert("error",true,response.data.message,5000);
+
+                    } else {
+
+                    this.circularLoader = false;
+                    this.setAlert("error",true,"There is internal server error",5000);
+                    }
+                }
+            }).catch((error)=>{
+
+                this.circularLoader = false;
+                if(error.response.data.generalErrorCode){
+                this.setAlert("error",true,error.response.data.message,10000);
+                } else {
+                this.setAlert("error",true,"Client: There is internal error",10000);
+                }
+            });
+        },
+
         viewOffense(item){
             this.offense.name = item.offence.name;
             this.offense.description = item.description;
             this.offense.action = item.offence.offence_type.offence_type_action.action;
             this.offense.type = item.offence.offence_type.name;
-          
-            const status = item.processes[item.processes.length - 1].status;
-            console.log(status);
+
 
             if(item.processes !=null && item.processes.length > 0){
+
+                const status = item.processes[item.processes.length - 1].status;
 
                 if(status == "REPORTED" || status == "NOTIFIED"){
 
@@ -206,8 +374,8 @@ export default {
             }
                 
             
-            if(item.resolution !=null && item.resolution.length > 0){
-                this.offense.resolution = item.offence.resolution;
+            if(item.resolution !=null){
+                this.offense.resolution = item.resolution; 
             }
                 
 
